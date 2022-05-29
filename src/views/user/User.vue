b21d6cb3bc236d95dd5dc3ac36fc6e957f228dd1
xin.li <xin.li@quncaotech.com>
2022-05-29 13:55:45 +0800

    feat: feat: 新增动态路由 2.引入全局变量

--- src/views/user/User.vue
+++ src/views/user/User.vue
@@ -5,7 +5,7 @@
 
 <script setup lang="ts">
 /* 引入全局el-element时，需要引入样式,否则样式会错乱 */
-// import 'element-plus/es/components/message-box/style/css'
+import 'element-plus/es/components/message-box/style/css'
 import { ComponentInternalInstance, getCurrentInstance } from 'vue' // 添加断言
 const { proxy } = getCurrentInstance() as ComponentInternalInstance
 const add = () => {
